35068edbc73c6a194b5f559bf334e43adffec9bd
bsusens-jackson <bsusens-jackson@zipari.com>
2019-04-23 13:06:38 -0400

    Adds separate log formatting for stdout and json

--- vladiate/vlad.py
+++ vladiate/vlad.py
@@ -1,48 +1,63 @@
 from __future__ import division
 import csv
 from collections import defaultdict
+import time
+
 from vladiate.exceptions import ValidationException
 from vladiate.validators import EmptyValidator
 from vladiate import logs
 
 
-class Vlad(object):
-    def __init__(
-        self,
-        source,
-        validators={},
-        default_validator=EmptyValidator,
-        delimiter=None,
-        ignore_missing_validators=False,
-    ):
+class LogFormatter:
+    def __init__(self):
         self.logger = logs.logger
-        self.failures = defaultdict(lambda: defaultdict(list))
-        self.missing_validators = None
-        self.missing_fields = None
-        self.source = source
-        self.validators = validators or getattr(self, "validators", {})
-        self.delimiter = delimiter or getattr(self, "delimiter", ",")
-        self.line_count = 0
-        self.ignore_missing_validators = ignore_missing_validators
 
-        self.validators.update(
-            {
-                field: [default_validator()]
-                for field, value in self.validators.items()
-                if not value
-            }
-        )
+    def log_debug_failures(self, failures):
+        raise NotImplementedError
 
-    def _log_debug_failures(self):
-        for field_name, field_failure in self.failures.items():
+    def log_validator_failures(self, validators):
+        raise NotImplementedError
+
+    def log_missing_validators(self):
+        raise NotImplementedError
+
+
+class JsonLogFormatter(LogFormatter):
+    def __init__(self):
+        super().__init__()
+
+    def log_validator_failures(self, validators):
+        for field_name, validators_list in validators.items():
+            for validator in validators_list:
+                if validator.bad:
+                    self.logger.error(
+                        {'validator': validator.__class__.__name__,
+                         'total_failures': validator.fail_count,
+                         'field_name': field_name,
+                         'timestamp': int(time.time()),
+                         'invalid_fields': list(validator.bad)})
+
+    def log_debug_failures(self, failures):
+        pass
+
+    def log_missing_validators(self):
+        pass
+
+
+class StdoutLogFormatter(LogFormatter):
+    def __init__(self):
+        super().__init__()
+
+    def log_debug_failures(self, failures):
+        for field_name, field_failure in failures.items():
             self.logger.debug('\nFailure on field: "{}":'.format(field_name))
             for i, (row, errors) in enumerate(field_failure.items()):
                 self.logger.debug("  {}:{}".format(self.source, row))
                 for error in errors:
                     self.logger.debug("    {}".format(error))
 
-    def _log_validator_failures(self):
-        for field_name, validators_list in self.validators.items():
+    def log_validator_failures(self, validators):
+        for field_name, validators_list in validators.items():
             for validator in validators_list:
                 if validator.bad:
                     self.logger.error(
@@ -69,15 +84,15 @@ class Vlad(object):
                     except TypeError:
                         pass
 
-    def _log_missing_validators(self):
+    def log_missing_validators(self):
         self.logger.error("  Missing validators for:")
-        self._log_missing(self.missing_validators)
+        self.log_missing(self.missing_validators)
 
-    def _log_missing_fields(self):
+    def log_missing_fields(self):
         self.logger.error("  Missing expected fields:")
-        self._log_missing(self.missing_fields)
+        self.log_missing(self.missing_fields)
 
-    def _log_missing(self, missing_items):
+    def log_missing(self, missing_items):
         self.logger.error(
             "{}".format(
                 "\n".join(
@@ -85,6 +100,44 @@ class Vlad(object):
                 )
             )
         )
+
+
+class Vlad(object):
+    def __init__(
+        self,
+        source,
+        validators={},
+        default_validator=EmptyValidator,
+        delimiter=None,
+        ignore_missing_validators=False,
+        log_format='stdout'
+    ):
+        self.logger = logs.logger
+        self.failures = defaultdict(lambda: defaultdict(list))
+        self.missing_validators = None
+        self.missing_fields = None
+        self.source = source
+        self.validators = validators or getattr(self, "validators", {})
+        self.delimiter = delimiter or getattr(self, "delimiter", ",")
+        self.line_count = 0
+        self.log_format = log_format
+        self.ignore_missing_validators = ignore_missing_validators
+
+        self.validators.update(
+            {
+                field: [default_validator()]
+                for field, value in self.validators.items()
+                if not value
+            }
+        )
+
+        self.log_formatter = self._instantiate_formatter()
+
+    def _instantiate_formatter(self):
+        log_formats = frozenset(('json', 'stdout'),)
+        assert self.log_format in log_formats, 'unrecognized log format'
+        _class_name = '{0}LogFormatter'.format(self.log_format.capitalize())
+        return globals()[_class_name](source=self.source)
 
     def validate(self):
         self.logger.info(
